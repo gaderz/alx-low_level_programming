bc27c8dc6c4af02935de0dad239f518a5a42aecf
gaderz <nonnygaders@gmail.com>
2023-02-16 14:29:15 -0800

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(s));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
 return (0);
 }
