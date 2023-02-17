4a7c6574e3f86c46c09b4d0b834bbe65d2d5792e
gaderz <nonnygaders@gmail.com>
2023-02-17 08:06:25 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'A program that prints alph in l.case'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	Return (0);
+}
