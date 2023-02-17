39bdaf1a123e407131571a90a835596c634d5259
gaderz <nonnygaders@gmail.com>
2023-02-17 00:33:33 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'writing a program that prints a line to the standard error'
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
