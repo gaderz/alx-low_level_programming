9ed9a46bdf8d9b7a3151a2c1ca8e6ba351283ac3
gaderz <nonnygaders@gmail.com>
2023-02-17 09:01:48 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print int number from zero - ten'
+ * Return: Always 0 (Suceess)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; P++)
+	{
+	printf("%d\n", p);
+	}
+	return (0);
+}
