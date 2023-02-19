419936f4eecf69b7eda08bfc67dbb4d3db246802
gaderz <nonnygaders@gmail.com>
2023-02-19 01:54:09 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Nested Loop of three digits of numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 8)
+	{
+		int b = a + 1;
+
+	while (b < 9)
+	{
+		int c = B + 1;
+
+	while (c < 10)
+	{
+		putchar('0' + a);
+		putchar('0' + b);
+		putchar('0' + c)
+			if (a < 7 || b < 8 || c < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		c++;
+	}
+	b++;
+	}
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
