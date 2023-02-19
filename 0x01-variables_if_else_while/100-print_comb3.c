24d8ad3f519cb13e79c8a0c281e2ddc014e05be3
gaderz <nonnygaders@gmail.com>
2023-02-19 01:25:46 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints too digits together from ten to hundred'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l = o;
+
+	while (l < 10)
+	{
+		int h = l + 1;
+
+		while (h < 10)
+		{
+			putchar(l + '0');
+			putchar(h + '0');
+
+			if (l != 8 || h != 9)
+			{
+				putchar(',');
+				putchar(' ')
+			}
+			h++;
+		}
+		l++;
+	}
+	return (0);
+}
