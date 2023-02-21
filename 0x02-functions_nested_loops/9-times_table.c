6473030a687f79e2a5132b6f672f23a8dede487f
gaderz <nonnygaders@gmail.com>
2023-02-21 18:50:09 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - time table being printed in table format
+ * Return: alwats success
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	result = i * j;
+	if (result < 10)
+	{
+	_putchar(result + '0');
+	if (j < 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	}
+	else
+	{
+		_putchar(result / 10 + '0');
+		_putchar(result % 10 + '0');
+		if (j < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	}
+	_putchar('\n');
+	}
+}
