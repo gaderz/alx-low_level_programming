72dd940654bf885478cebbd7a1d97847b69ddcc5
gaderz <nonnygaders@gmail.com>
2023-02-22 10:12:59 +0000

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Description: 'computing the natural numbers'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int sum = 0, a;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum += a;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
