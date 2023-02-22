2531aedbedfd8eb3dc44f752f94da00e6d399a05
gaderz <nonnygaders@gmail.com>
2023-02-22 13:58:05 +0000

    fixing my output

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,15 +21,15 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	fib1_half1 = fib1 / 1000000000;
-	fib2_half1 = fib2 / 1000000000;
-	fib1_half2 = fib1 % 1000000000;
-	fib2_half2 = fib2 % 1000000000;
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if (half2 >= 10000000000)
+		if (fib1_half1 + fib2_half2 > 9999999999)
 		{
 		half1 += 1;
 		half2 %= 10000000000;
