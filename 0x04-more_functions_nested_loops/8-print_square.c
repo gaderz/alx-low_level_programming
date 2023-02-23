79f1bf844afef10e7926483f3c0d64e8170bcbda
gaderz <nonnygaders@gmail.com>
2023-02-23 13:47:00 +0000

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * void print_square - printing the size of a square
+ * @size: number of square to be printed
+ * Return: Always success
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
