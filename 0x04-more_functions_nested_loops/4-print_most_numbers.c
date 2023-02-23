9380e7444c77dc9729c58bbf49b183d8854746ce
gaderz <nonnygaders@gmail.com>
2023-02-23 11:33:17 +0000

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - looping and omitting base 2 and 4
+ *
+ * Retuen: Success
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
