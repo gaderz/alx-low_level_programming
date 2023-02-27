89852ab48e5d893ae32aab044f2458e4ec6568d2
gaderz <nonnygaders@gmail.com>
2023-02-27 16:00:14 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap twi integers
+ * @a: value 1 to be swapped
+ * @b: value 2 to be swapped
+ * Return: Always 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
