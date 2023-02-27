04b7de8df1a5f9ca1d879d27d16f3a2b1fb0deac
gaderz <nonnygaders@gmail.com>
2023-02-27 17:58:39 +0000

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints string, starting with the first
+ * @str: the string to print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+}
