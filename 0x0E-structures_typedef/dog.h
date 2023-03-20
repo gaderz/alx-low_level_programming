fd24a0981f49d2de72fd006251e96cca23140af2
gaderz <nonnygaders@gmail.com>
2023-03-20 14:49:47 +0000

    a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure representing a dog
+ * @name: a pointer to the dog's name
+ * @age: the dog's age as a float
+ * @owner: a pointer to the dog's owner's name
+ *
+ * Description: this struct contains information about a dog
+ */
 typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 } dog_t;
+
+#endif /* DOG_H */
+
